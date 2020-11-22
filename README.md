6a21d58d1705c3f9e034773226f6d5b4fb349908
Alexandr Voitsekhovskyy <alexandrvoits@gmail.com>
2020-11-22 15:36:06 +0200

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # tic-tac-toe_gameConsole
 Simple Tic-Tac-toe game for 2 players via console. (study task)
+
+Rules same as for default tic-tac-toe, but:
+You and your opponent have to press 1-9 (and nothing else) button in turn. 
+Cross always moves first.
+
+Possible results:
+Cross win.
+Zero win.
+Draw.
+
+Note, please:
+This is my first "one-night project" on C# made as a home task for courses.
+Nothing special for novice (accept "State" Pattern).
---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    //possible cell condition
+    public enum State
+    {
+        Cross,
+        Zero,
+        Unset
+    }
+    //possible game winner
+    public enum Winner
+    {
+        Crosses,
+        Zeroes,
+        Draw,
+        GameIsUnfinished
+    }
+
+    //class made for game logic only
+    class TicTacToeGame
+    {
+        private State[] board = new State[9];//one-dimensional array for our board
+        public int MovesCounter { get; private set; }
+
+        //fill our board with "Unset" status
+        public TicTacToeGame()
+        {
+            for(int i=0; i < board.Length; i++)
+            {
+                board[i] = State.Unset;
+            }
+        }
+        
+        public void MakeMove(int index)
+        {
+            board[index - 1] = MovesCounter % 2 ==0 ? State.Cross : State.Zero;//our index will start from 1 to make it more simple for users
+
+            MovesCounter++;
+        }
+
+        public State GetState(int index)
+        {
+            return board[index - 1];
+        }
+
+        public Winner GetWinner()
+        {   
+            //winning combinations
+            return GetWinner(1,4,7,2,5,8,3,6,9,
+                1,2,3,4,5,6,7,8,9,
+                1,5,9, 3,5,7);
+        }
+
+        //finding the winner
+        private Winner GetWinner(params int[] indexes)
+        {
+            for(int i=0; i < indexes.Length; i += 3)
+            {
+                bool same = AreSame(indexes[i], indexes [i+1], indexes[i+2]);
+                
+                //our status has not be "Unset"
+                if (same)
+                {
+                    State state = GetState(indexes[i]);
+                    if (state != State.Unset)
+                    {
+                        return state == State.Cross ? Winner.Crosses : Winner.Zeroes;
+                    }
+                }
+            }
+            if(MovesCounter < 9)
+            {
+                return Winner.GameIsUnfinished;
+            }
+            return Winner.Draw;
+        }
+
+        //method helps us to find the same cell status
+        private bool AreSame(int a, int b, int c)
+        {
+            return GetState(a) == GetState(b) && GetState(a) == GetState(c);
+        }
+    }
+}
